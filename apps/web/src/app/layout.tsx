e0f1e37c3bd1f205c761ac093efacc606b17ea90
vs0usa <reydux95@gmail.com>
2025-12-16 00:38:19 +0100

    feat(web): add dark theme

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from "next-themes"
 import type { ReactNode } from "react"
 
 import "./globals.css"
 
 export default function RootLayout(props: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>{props.children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class">{props.children}</ThemeProvider>
+      </body>
     </html>
   )
 }
