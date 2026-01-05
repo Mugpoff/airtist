fe5582bb55c8dcff63b32dc956317ea44ef1caa8
Priveetee <brahim.benyoucefpro@proton.me>
2026-01-05 21:13:31 +0100

    refactor(trpc): update drive router for nested handlers

--- packages/trpc/src/routers/drive.ts
+++ packages/trpc/src/routers/drive.ts
@@ -1,10 +1,10 @@
-import { driveConnectUrlHandler } from "../handlers/drive-connect-url-handler"
-import { driveGetThumbUrlHandler } from "../handlers/drive-get-thumb-url-handler"
-import { driveImportImagesHandler } from "../handlers/drive-import-images-handler"
-import { driveListConnectionsHandler } from "../handlers/drive-list-connections-handler"
-import { driveListFoldersHandler } from "../handlers/drive-list-folders-handler"
-import { driveListItemsHandler } from "../handlers/drive-list-items-handler"
-import { driveStatusHandler } from "../handlers/drive-status-handler"
+import { driveConnectUrlHandler } from "../handlers/drive/connect-url"
+import { driveGetThumbUrlHandler } from "../handlers/drive/get-thumb-url"
+import { driveImportImagesHandler } from "../handlers/drive/import-images"
+import { driveListConnectionsHandler } from "../handlers/drive/list-connections"
+import { driveListFoldersHandler } from "../handlers/drive/list-folders"
+import { driveListItemsHandler } from "../handlers/drive/list-items"
+import { driveStatusHandler } from "../handlers/drive/status"
 import { createTRPCRouter } from "../trpc"
 
 export const driveRouter = createTRPCRouter({
