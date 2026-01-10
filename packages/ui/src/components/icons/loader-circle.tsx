7c2040e668b9446b9ca85d73c268737e7df320d7
vs0usa <reydux95@gmail.com>
2026-01-10 10:17:28 +0100

    fix(ui): update loader-circle icon attributes

--- packages/ui/src/components/icons/loader-circle.tsx
+++ packages/ui/src/components/icons/loader-circle.tsx
@@ -13,9 +13,9 @@ export const LoaderCircleIcon = ({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={cn(
         "lucide lucide-loader-circle-icon lucide-loader-circle",
         className,
